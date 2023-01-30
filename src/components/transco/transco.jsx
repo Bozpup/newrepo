126f833c86cac4d2c786c1b84b48940f5bb8e002
SireDCA <ACHONU7570@student.babcock.edu.ng>
2023-01-30 23:37:54 +0100

    Added transco component and removed boilerplate

---
+++ src/components/transco/transco.jsx
@@ -0,0 +1,18 @@
+import {react} from 'react'
+import "./transco.css"
+
+export const Transco = () => {
+    return (
+        <>
+            <section className="bus-transco">
+                <div>
+                    <article>
+                        <h4>Are you a Bus Transco?</h4>
+                        <p>Onboard and gain access to services that will boost your business</p>
+                        <p><a href="#">Sign up now</a></p>
+                    </article>
+                </div>
+            </section>
+        </>
+    )
+}
