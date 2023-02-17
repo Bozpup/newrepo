6326186cb94b2fa75be4282c5eb5dd9c02bc0d40
Ahmed Bolarinwa <bolarinwaahmed22@gmail.com>
2023-02-17 11:04:21 +0100

    Worked on refactoring the landing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { register } from "swiper/element/bundle";
 import ForgotPassword from "./pages/ForgotPassword";
 import LandingPage from "./pages/LandingPage";
-import PasswordVerification from './pages/PasswordVerification';
+import PasswordVerification from "./pages/PasswordVerification";
 
 function App() {
+  register();
+
   return (
     <div className="app">
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/password_verification" element={<PasswordVerification />} />
+          <Route
+            path="/password_verification"
+            element={<PasswordVerification />}
+          />
           <Route path="/forgot_password" element={<ForgotPassword />} />
         </Routes>
       </Router>
--- src/utils/Images.js
+++ src/utils/Images.js
@@ -1,11 +1,13 @@
 import danfo from "./../assets/popularTrips/danfoBus.png";
 import buses from "./../assets/popularTrips/buses.png";
 import napep from "./../assets/popularTrips/napep.png";
+import star from './../assets/popularTrips/star.png';
 
 const Images = {
   danfo,
   buses,
   napep,
+  star
 };
 
 export default Images;
